5fb9a2f20f525257121a781bc3c58e3dbcce7f40
jovanialferez <jovanialferez@users.noreply.github.com>
2024-02-24 02:07:58 +0000

    fix: add missing iteration key

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -16,7 +16,7 @@
         </thead>
         <tbody>
             @foreach($users as $user)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr wire:key="{{ $user->id }}" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {{ $user->id }}
                 </th>
